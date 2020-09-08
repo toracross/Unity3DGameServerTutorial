8f5fc8808190e76416ed475fd0bdf353d9551750
Wellison Pereira <toracross@icloud.com>
2020-09-07 23:36:08 -0400

    Initialize Repo

---
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    public GameObject playerPrefab;
+
+    #region Instance
+    public static NetworkManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+    #endregion
+
+    private void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 30;
+        Server.Start(50, 26950);
+    }
+
+    private void OnApplicationQuit()
+    {
+        Server.Stop();
+    }
+
+    public Player InstantiatePlayer()
+    {
+        Vector3 spawnPosition = new Vector3(0f, 0.5f, 0f);
+        return Instantiate(playerPrefab, spawnPosition, Quaternion.identity).GetComponent<Player>();
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Unity3D Dedicated Server Tutorial - Server
+
+This is the server portion of the Unity3D Dedicated Server Tutorial.
+
+This should be used in conjunction with the client.
+https://github.com/toracross/Unity3DGameClientTutorial
